925ae504c43483fb717d31e73371aa947264e509
Demassala <dubrel.demassala@gmail.com>
2024-04-10 03:19:03 +0200

    Filtrer Clusiaceae

--- R_phenologie.R
+++ R_phenologie.R
@@ -12,3 +12,6 @@ data_Vouacapoua_americana
 # Filtrer les données pour n'afficher que l'espèce "Symphonia globulifera"
 data_Symphonia_globulifera <- subset(data, Species == "globulifera")
 data_Symphonia_globulifera
+#Filtrer les données pour n'afficher que les Clusiaceae
+data_Clusiaceae <-subset(data, Family == "Clusiaceae")
+data_Clusiaceae
