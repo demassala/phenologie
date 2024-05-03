bf61f5632ae1bfb8facdba6cdfe91cd208df28ca
Demassala <dubrel.demassala@gmail.com>
2024-05-03 22:58:41 +0200

    graphiques phenophage

--- R_phenologie.R
+++ R_phenologie.R
@@ -116,4 +116,136 @@ pivot_longer(
   values_to = "phenophases") %>%
   print()->
   synthese_tab1
+## Filtrer que les Symphonia globulifera
+synthese_tab1 %>%
+  filter(Genus=="Symphonia" & Species=="globulifera") %>%
+  select (Family,Species,Genus, date,phenophases) %>%
+  print()-> synthese_Sympho
+
+
+               # Floraison du flux de travail
+#Nous n’acceptons d’évaluer le flux de travail de floraison que si nous
+#avons au moins trois événements de floraison par datechez tous les individus de l’espèce
+
+              # premiers calculs
+n_event_flo_spec = pheno2 %>%
+  filter(Genus_Spec =="Symphonia globulifera") %>%
+  distinct(CrownID,date,PPFlo) %>%
+  group_by(CrownID,PPFlo) %>%
+  summarise(n = n()) %>% filter(PPFlo == "Fl") %>% ungroup() %>% pull(n) %>% sum()
+
+              #Schéma général
+Leaf_Pattern(Data =pheno2  %>% filter(Usable ==1), Obs_Veg ="PPVeg", Spec = "Symphonia globulifera", fertility = TRUE)[[2]]
+
+         #Temps des phénophases
+          ##Temps de séjour
+    # C’est la différence entre le début d’un événement et la fin du même événement
+     PhenoPhase_Time(
+       Data = pheno2,
+       Pattern = "Fl",
+       Spec = "Symphonia globulifera",
+       Obs_Veg = "PPFlo",
+       markers = "Residence_time"
+     )[[2]]
+     ### Temps de retour
+   ## C’est la différence entre la fin d’un événement et le début de l’événement suivant
+     PhenoPhase_Time(
+       Data = pheno2,
+       Pattern = "Symphonia globulifera",
+       Spec = ,
+       Obs_Veg = "PPFlo",
+       markers = "Return_time"
+     )[[2]]
+   ## Proportion d’individus dans une floraison par date
+     GraphPropF <- LeafedOTim(Data=pheno2,
+                              Spec= "Symphonia globulifera",
+                              Pattern=c("Fl"),
+                              Obs_Veg = "PPFlo")
+     GraphPropF[[2]]
+
+## Proportion de phénophase
+# par mois:
+ Il s’agit du nombre d’un phénophase donné à chaque mois par an.
+  Nous regroupons d’abord par espèce, par année et après mois et après nous
+  calculons le nombre de phénophases d’événement ensuite, le nombre total de
+   phénophases par mois et ensuite, la proportion de phénophases données.
+
+   Nous comptons donc :
+     $$ \frac{\text{number of the given phenophases in a month}}{\text{all
+       phenophases in the same month}}$$
+Si une personne a deux Fl obs au cours d’un même mois, nous comptons comme deux.
+
+   Leaf_Circular(Data = Full, Spec = WantedSpec,Pattern = c("Fl"),
+                 Obs_Veg = "PPFlo",perYears = FALSE)[[2]]
+
+  # par mois et par année
+   Leaf_Circular(Data = Full, Spec = WantedSpec,Pattern = c("Fl"),
+                 Obs_Veg = "PPFlo",perYears = TRUE)[[2]]
+
+ ## Proportion d’individus dans une phénophase donnée
+   Nous détectons le pic (début, max, fin) en fonction des
+   paramètres et du signal traité (Moyenne mobile)
+
+   # signal
+
+   data_signal = LeafedOTim(Data=pheno2 %>% filter(Usable==1),
+                            Spec= "Symphonia globulifera",
+                            Pattern=c("Fl"),
+                            Obs_Veg = "PPFlo")[[1]]
+
+   signal = data_signal %>% select(prop) %>% pull()
+   moyenne_mobile <- moving_average(data_signal %>% select(prop) %>%
+                                      pull(),filter = fpoids(n=2,p=2,q=2)$y)
+   #Vous pouvez régler le nombre de voisines pour le signal traité sur les paramètres "n"
+
+
+   #   On indentifie les différents pics positif et négatif
+
+   dates = data_signal %>% select(date) %>% pull() # Extracting the differents dates
+   # the maximum of pics
+   dates_max = sort(findpeaks(moyenne_mobile,minpeakheight  = 10,nups =1)[,2])
+   # When the pics begin
+   dates_begin =sort(findpeaks(moyenne_mobile,minpeakheight  = 10,nups = 1)[,3])
+   # When the pics end
+   dates_end = sort(findpeaks(moyenne_mobile,minpeakheight  = 10,nups = 1)[,4])
+   # Percent of ind by peaks
+   amplitude_peaks = findpeaks(moyenne_mobile,minpeakheight  = 10,nups = 1)[,1]
+
+   #Il faut ensuite calculer l’amplitude réelle des crêtes
+           déterminées sur le signal traité
+
+           amplitude_real = signal[dates_max]
+
+   ## Tracé du signal d'origine, de la moyenne mobile et de la dérivée avec ggplot2
+           Plot = ggplot(data_signal, aes(x = date)) +
+             geom_line(aes(y = prop, color = "original signal")) +
+             geom_line(aes(y = moyenne_mobile, color = "processed signal"))+
+             geom_point(aes(y = prop, color = "original signal")) +
+             scale_color_manual(values = c("original signal" = "blue","processed signal" = "red"))+
+             theme(axis.text.x = element_text(angle = 90),
+                   panel.background = element_rect(fill = "white")) +
+             geom_vline(xintercept = dates[dates_max],col = "tomato3" , linetype = "dashed") +
+             geom_vline(xintercept = dates[dates_begin],col = "grey30", linetype = "dashed") +
+             geom_vline(xintercept = dates[dates_end],col = "grey30", linetype = "dashed") +
+             scale_x_date(date_breaks = "2 month", date_labels = "%b-%Y") +
+             labs(title = "original and processed signal by Moving average", x = "Time", y = "Value") +
+             annotate("text",x = dates[dates_max], y= 100,label = paste(round(amplitude_real,1),"%"),col = "grey40")
+
+           Plot
+
+           Leaf_Pattern(Data = Full %>% filter(Usable ==1), Obs_Veg ="PPVeg", Spec = WantedSpec, fertility = TRUE)[[2]] +    geom_vline(xintercept = dates[dates_max], col = "white" , linetype = "dashed") +
+             geom_vline(xintercept = dates[dates_begin], col = "black" , linetype = "dashed") +
+             geom_vline(xintercept = dates[dates_end], col = "black" , linetype = "dashed")
+
+           summary_table = tibble(Genus_Spec = data_signal$Genus_Spec %>% unique(),
+                                  max = dates[dates_max],
+                                  range = abs(difftime(dates[dates_begin],dates[dates_end])),
+                                  start = dates[dates_begin],
+                                  end = dates[dates_end]
+           )
+
+           kable(summary_table)
+
+
+           ```
 
